8bcccce89c4d96875f959b1878643353403002a1
mahima-ins <paudelmahima4@gmail.com>
2023-10-25 22:58:25 +0545

    login and register page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Home from './Home'
+import Login from './Components/Login'
+import Register from './Components/Register'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
  const App = () => {
   return (
     <>
-    <Home/>
+    
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home/>}/> 
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
       </>
   )
 }
