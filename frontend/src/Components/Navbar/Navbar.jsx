db6d8089e2f5bc40774bab03aaaca1acfb6782f9
mahima-ins <paudelmahima4@gmail.com>
2023-10-25 12:05:37 +0545

    navbar created

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+      <div className="navbar">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Lend</a>
+      <a href="#">Rent</a>
+      <a href="#">Login</a>
+  </div>
+  
+    )
+  }
+  
+  export default Navbar
