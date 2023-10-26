2a30bc8d3b0ec4ef86cc26ee514746c212901790
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2023-10-26 08:13:14 +0545

    create a user signup model and route

---
+++ Backend/controllers/user.js
@@ -0,0 +1,14 @@
+ const User = require('../models/User')
+ 
+ const signupUser= async (req,res)=>{
+    try {
+        const user = req.body;
+        const  newUser = new User(user);
+        await newUser.save();
+        return res.status(200).json({msg: "Signup successfull"})
+    } catch (error) {
+        return res.status(500).json({msg:"Error while signup the user "})
+    }
+
+}
+module.exports = signupUser;
---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const connectToMongo = require('./database/db');
+const router = require('./routes/route')
+const cors = require('cors');
+const bodyparser = require('body-parser')
+
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+// Body-parser middleware
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+
+// available routes
+app.use('/',router)
+
+connectToMongo().then(()=>{
+try {
+    app.listen(PORT,()=>{
+        console.log(`Server listening on port http://localhost:${PORT}`);
+    })
+    
+} catch ( error) {
+    console.log("Cannot connect to the server")
+}
+}).catch( ( error )=>{
+    console.log("Invalid database connection")
+})
