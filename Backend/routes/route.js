626ca449d9fc7c1aafd668f39bbf27ef35c6ad0a
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2023-11-01 10:47:57 +0545

    connecting backend and client for login and signup page

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const {signupUser,loginUser} = require('../controllers/user')
-const validateUserData = require('../middleware/validateUserData')
+
 
 router.post('/signup',signupUser)
 router.post('/login',loginUser)
