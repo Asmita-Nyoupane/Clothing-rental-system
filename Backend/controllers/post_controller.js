58636fb91239b8cba263a27fb186027eb9ef92a8
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2023-11-21 15:04:05 +0545

    creatign routes to get single post, update and delte the post

--- Backend/controllers/post_controller.js
+++ Backend/controllers/post_controller.js
@@ -1,27 +1,76 @@
 const Post = require("../models/Post");
 
-const createPost = async (req,res)=>{
-    try {
-        const post = await new Post(req.body);
-        post.save();
-        return res.status(200).json({msg:"Post saved successfully"})
-    } catch (error) {
-        return res.status(500).json({msg:error})
+// create a new post
+
+const createPost = async (req, res) => {
+  try {
+    const post = await new Post(req.body);
+    post.save();
+    return res.status(200).json({ msg: "Post saved successfully" });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
+};
+
+// Get all available post
+
+const getAllPosts = async (req, res) => {
+  let category = req.query.category;
+  let posts;
+  try {
+    if (category) {
+      posts = await Post.find({ categories: category });
+    } else {
+      posts = await Post.find({});
     }
 
-}
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+};
 
+// Get the post of specific id
 
-const getAllPosts = async (req,res )=>{
-    try {
-        let posts = await  Post.find({});
-        return res.status(200).json(posts);
-        
-    } catch (error) {
-       return  res.status(500).json({
-            msg: error.message
-        });
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+// update the post
+
+const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
     }
-}
+    await Post.findByIdAndUpdate(req.params.id, { $set: req.body });
+    return res.status(200).json({ msg: "Post updated Successfully" });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
 
-module.exports = {createPost, getAllPosts};
+// delete the post
+
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    await Post.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ msg: "post deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+module.exports = { createPost, getAllPosts, getPost, updatePost, deletePost };
