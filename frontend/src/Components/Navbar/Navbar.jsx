b3424b31f1b17aaa8917cc44be5f11e968d21daa
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2023-12-21 19:03:06 +0545

    responsive navabar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,50 @@
-import './Navbar.css'
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import Search from '../Search/Search';
-//  import logo from '../../assets/logo.png'
-// import {DataContext} from '../../context/DataProvider'
+// Navbar.js
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Search from "../Search/Search";
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
-  
-  return (
-<> 
- <div className="navbar">
-    {/* <img src={logo} alt="Logo"/> */}
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/lend">Lend</Link>
-      <Link to="/rent">Rent</Link>
-      <Link to="/login">Login</Link>
-      <div className='search'>
-<Search/>
-</div>
-  
-    </div>
+  const [isMenuOpen, setMenuOpen] = useState(true);
 
-    
-    </>
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className={`navbar ${isMenuOpen ? "open" : ""}`}>
+      <img className="logo" src={logo} alt="Logo" />
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <div
+        className="menu-items"
+        style={{ display: isMenuOpen ? "flex" : "none" }}
+      >
+        <Link to="/" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link to="/about" onClick={toggleMenu}>
+          About
+        </Link>
+        <Link to="/lend" onClick={toggleMenu}>
+          Lend
+        </Link>
+        <Link to="/rent" onClick={toggleMenu}>
+          Rent
+        </Link>
+        <Link to="/login" onClick={toggleMenu}>
+          Login
+        </Link>
+      </div>
+      <div className="search">
+        <Search />
+      </div>
+    </div>
   );
 };
 
