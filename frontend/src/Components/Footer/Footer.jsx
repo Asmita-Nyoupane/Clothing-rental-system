a11d80f698d9cedf0fae1ae5ed1fdb1c823b9de8
mahima-ins <paudelmahima4@gmail.com>
2024-03-05 17:06:16 +0545

    modify footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,42 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import facebookImg from "../../assets/facebook.png";
+import instaImg from "../../assets/instagram.png";
+import LinkedinImg from "../../assets/linkedin.png";
+import TwitterImg from "../../assets/twitter.png";
+import logo from "../../assets/logo.png";
+
 
 const footer = () => {
   return (
     <footer
       style={{
-        backgroundColor: "rgba(100, 26, 56, 0.823)",
+        backgroundColor: "#8892b3",
         padding: "20px",
         marginTop: "20px",
         color: "white",
         textAlign: "center",
       }}
     >
+    
       <Link
         to="/about"
         style={{
           textDecoration: "none",
           color: "white",
-          marginLeft: "30px",
+           marginLeft: "-1100px",
         }}
       >
-        About
+        ABOUT US
       </Link>
       <Link
         to="/contact"
         style={{
           textDecoration: "none",
           color: "white",
-          marginLeft: "30px",
+          textAlign:"left",
+           marginLeft: "30px",
         }}
       >
-        Contact
+        Contact us
       </Link>
 
       <p>
         <b> Email : </b> <i>rentcycle@gmail.com</i>
       </p>
       <p>&copy; 2024 RentCycle</p>
+      {/* <p>Follow us</p> */}
+      <img
+              src={facebookImg}
+              style={{ width: "25px", height: "25px" }}
+              />
+              <img src={instaImg}
+               style={{ width: "25px", height: "25px", marginLeft:"15px" }}
+               />
+               <img src={LinkedinImg}
+               style={{ width: "31px", height: "31px", marginLeft:"15px" }}
+               />
+                <img src={TwitterImg}
+               style={{ width: "32px", height: "32px", marginLeft:"15px" }}
+               />
     </footer>
   );
 };
